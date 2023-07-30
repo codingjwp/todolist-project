e4743f842eb58cf89c5df627c4b8c273af93362a
codingjwp <codingjwp@gmail.com>
2023-07-30 11:43:35 +0900

    feat: useValidation 훅 적용

--- src/pages/SignInOfUp.tsx
+++ src/pages/SignInOfUp.tsx
@@ -1,24 +1,35 @@
+import { FormEvent, KeyboardEvent } from 'react';
 import { InputField } from '../components/InputField';
 import { Button } from '../components/Button';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { useValidation } from '../hooks/useValidation';
 
 interface SignInOfUpProps {
   titles: string
 }
 
 const SignInOfUp = ({titles}: SignInOfUpProps) => {
+  const [isEmail, isPassword, isDisable, changeEmailData, changePasswordData] = useValidation();
+  const PostSignInOfUpApi = (e: FormEvent) => {
+    e.preventDefault();
+    const path = titles.replace(/ /g, "") .toLowerCase();
+    // API 기능
+  }
+  
   return (
-    <SignInOfUpForm>
+    <SignInOfUpForm onSubmit={PostSignInOfUpApi}>
       <SignInOfUpTitle>{titles}</SignInOfUpTitle>
-      <SignInOfUpSpan>Email
-        <InputField aria-label='signinofup-input' type='text' />
-        <SignInOfUpWarring>이메일에 @를 포함시켜주세요.</SignInOfUpWarring>
+      <SignInOfUpSpan>
+        <SignInOfUpWarring $position='up'>Email</SignInOfUpWarring>
+        <InputField aria-label='signinofup-input' type='text' onChange={changeEmailData} />
+        <SignInOfUpWarring $visible={isEmail} >Email에 @를 포함시켜주세요.</SignInOfUpWarring>
       </SignInOfUpSpan>
-      <SignInOfUpSpan>Password
-        <InputField aria-label='signinofup-pw'type='password' />
-        <SignInOfUpWarring>비밀번호는 8자 이상이여야 합니다.</SignInOfUpWarring>
+      <SignInOfUpSpan>
+        <SignInOfUpWarring $position='up'>Password</SignInOfUpWarring>
+        <InputField aria-label='signinofup-pw'type='password' onChange={changePasswordData} />
+        <SignInOfUpWarring $visible={isPassword}>Password는 8자 이상입니다.</SignInOfUpWarring>
       </SignInOfUpSpan>
-      <Button aria-label='signinofup-btn' type='submit' $size='large' $btnType='primary'>{titles}</Button>
+      <Button aria-label='signinofup-btn' disabled={isDisable} type='submit' $size='large' $btnType='primary'>{titles}</Button>
     </SignInOfUpForm>
   );
 }
@@ -35,17 +46,22 @@ const SignInOfUpForm = styled.form`
 const SignInOfUpSpan = styled.span`
   width: 100%;
 `;
-const SignInOfUpWarring = styled.p`
-  margin-top: .3rem;
+const SignInOfUpWarring = styled.p<{$visible?: boolean, $position?: string}>`
+  
   font-weight: 540;
-  color: #B60000;
+  opacity: ${({$visible}) => $visible === true && 0 };
+  ${({$position}) => $position === 'up' ? css`
+    margin-bottom: .3rem;
+    color: #000000;
+  ` : css `
+    margin-top: .3rem;
+    color: #B60000;
+  `} 
 `
-
 const SignInOfUpTitle = styled.h1`
   font-size: 3rem;
   text-align: center;
   width: 100%;
 `
-
 
 export default SignInOfUp;
