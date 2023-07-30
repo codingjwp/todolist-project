3a38e5b6f5524e43934929bc3b994814b6bbe051
codingjwp <codingjwp@gmail.com>
2023-07-30 11:43:54 +0900

    feat: hook 적용

---
+++ src/hooks/useValidation.tsx
@@ -0,0 +1,23 @@
+import {ChangeEvent, useCallback, useMemo, useState } from 'react'
+
+export const useValidation = () =>{
+  const EMAIL_CHECK = /^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+  const [isEmail, setIsEmail] = useState(false);
+  const [isPassword, setIsPassword] = useState(false);
+  const isDisable = useMemo(() => {
+    return !(isEmail && isPassword);
+  }, [isEmail, isPassword]) 
+
+  const changeEmailData = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    const check = EMAIL_CHECK.test(e.currentTarget.value);
+    if (check) setIsEmail(true);
+    else if (!check && isEmail) setIsEmail(false); 
+  }, [isEmail]);
+  const changePasswordData = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    const check = e.currentTarget.value.length > 7;
+    if (check) setIsPassword(true);
+    else if (!check && isPassword) setIsPassword(false);
+  }, [isPassword]);
+
+  return [isEmail, isPassword, isDisable, changeEmailData, changePasswordData] as const
+}
