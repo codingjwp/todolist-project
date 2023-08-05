57ca3eecd35578102e448d13bd2073542eb6f295
codingjwp <codingjwp@gmail.com>
2023-08-06 00:33:54 +0900

    feat: localhost 값이 존재 할 경우 todo, signin으로 이동 기능

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, Outlet, redirect } from 'react-router-dom';
 import GlobalStyles from './styles/GlobalStyles';
 import Layout from './styles/Layout';
 import Home from './pages/Home';
@@ -6,6 +6,13 @@ import SignInOfUp from './pages/SignInOfUp';
 import Todos from './pages/Todos';
 import Modal from './components/Modal';
 import { useModalState } from './apis/ModalContext';
+
+const hasToken = async (path: string) => {
+  const token = localStorage.getItem('access_token') ? true : false;
+  if (path !== 'todo' && token) return redirect('/todo');
+  else if (path === 'todo' && !token) return redirect('/signin');
+  return false;
+}
 
 export const routerElement = [
   {
@@ -16,10 +23,10 @@ export const routerElement = [
       </Layout>),
     errorElement: <div>error</div>,
     children: [
-      { index: true, element: <Home /> },
-      { path: 'signup', element: <SignInOfUp titles='Sign Up' /> },
-      { path: 'signin', element: <SignInOfUp titles='Sign In' /> },
-      { path: 'todo', element: <Todos /> },
+      { index: true, element: <Home />, loader: async () => {return await hasToken('/')} },
+      { path: 'signup', element: <SignInOfUp titles='Sign Up' />, loader: async () => {return await hasToken('signup')} },
+      { path: 'signin', element: <SignInOfUp titles='Sign In' />, loader: async () => {return await hasToken('signin')} },
+      { path: 'todo', element: <Todos />, loader: async () => {return await hasToken('todo')} },
   ]}
 ];
 
