a12829cf89062e4fba129a227c70b1a661786138
codingjwp <codingjwp@gmail.com>
2023-08-15 13:37:06 +0900

    test: husky 및 lint-staged 테스트

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { StrictMode } from 'react';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-<StrictMode>
+  <StrictMode>
     <App />
-</StrictMode>
+  </StrictMode>,
 );
